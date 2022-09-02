1853cf29c56c933f1dc5eb83d33469c118cc36ea
unknown <andmih666@gmail.com>
2022-09-02 17:15:50 +0300

    added new function swap() for optimization

--- 2_task.php
+++ 2_task.php
@@ -3,23 +3,27 @@
 // Написать функцию сортировки простого численного массива, не используя стандартные функции сортировки.
 // В качестве аргументов передавать сам массив и тип сортировки ASC/DESC.
 
+function swap()
+{
+  global $array, $i;
+  $tmp_var = $array[$i + 1];
+  $array[$i + 1] = $array[$i];
+  $array[$i] = $tmp_var;
+  return $array;
+}
+
 function sorting_array($array, $sorting_type)
 {
-  $array = explode(' ', readline("Enter array values through spaces and hit Enter: "));
-  $sorting_type = readline("Choose sorting type: enter 'asc' for ascending, 'desc' for descending: ");
+  global $array, $i, $sorting_type;
   for ($x = 0; $x < count($array) - 1; $x++) {
     for ($i = 0; $i < count($array) - $x - 1; $i++) {
       if ($sorting_type == 'asc') {
         if ($array[$i] > $array[$i + 1]) {
-          $tmp_var = $array[$i + 1];
-          $array[$i + 1] = $array[$i];
-          $array[$i] = $tmp_var;
+          swap();
         }
       } elseif ($sorting_type == 'desc') {
         if ($array[$i] < $array[$i + 1]) {
-          $tmp_var = $array[$i + 1];
-          $array[$i + 1] = $array[$i];
-          $array[$i] = $tmp_var;
+          swap();
         }
       } else {
         break;
@@ -28,3 +32,9 @@ function sorting_array($array, $sorting_type)
   }
   return $array;
 }
+
+// проверяем работу
+$array = explode(' ', readline("Enter array values through spaces and hit Enter: "));
+$sorting_type = readline("Choose sorting type: enter 'asc' for ascending, 'desc' for descending: ");
+sorting_array($array, $sorting_type);
+print_r($array);
