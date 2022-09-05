e66ddc535db33d3c87560b0f088f5ac83fee2b8c
unknown <andmih666@gmail.com>
2022-09-05 13:31:03 +0300

    Revert "added new function swap() for optimization"
    This reverts commit 1853cf29c56c933f1dc5eb83d33469c118cc36ea.

--- 2_task.php
+++ 2_task.php
@@ -3,27 +3,23 @@
 // Написать функцию сортировки простого численного массива, не используя стандартные функции сортировки.
 // В качестве аргументов передавать сам массив и тип сортировки ASC/DESC.
 
-function swap()
-{
-  global $array, $i;
-  $tmp_var = $array[$i + 1];
-  $array[$i + 1] = $array[$i];
-  $array[$i] = $tmp_var;
-  return $array;
-}
-
 function sorting_array($array, $sorting_type)
 {
-  global $array, $i, $sorting_type;
+  $array = explode(' ', readline("Enter array values through spaces and hit Enter: "));
+  $sorting_type = readline("Choose sorting type: enter 'asc' for ascending, 'desc' for descending: ");
   for ($x = 0; $x < count($array) - 1; $x++) {
     for ($i = 0; $i < count($array) - $x - 1; $i++) {
       if ($sorting_type == 'asc') {
         if ($array[$i] > $array[$i + 1]) {
-          swap();
+          $tmp_var = $array[$i + 1];
+          $array[$i + 1] = $array[$i];
+          $array[$i] = $tmp_var;
         }
       } elseif ($sorting_type == 'desc') {
         if ($array[$i] < $array[$i + 1]) {
-          swap();
+          $tmp_var = $array[$i + 1];
+          $array[$i + 1] = $array[$i];
+          $array[$i] = $tmp_var;
         }
       } else {
         break;
@@ -32,9 +28,3 @@ function sorting_array($array, $sorting_type)
   }
   return $array;
 }
-
-// проверяем работу
-$array = explode(' ', readline("Enter array values through spaces and hit Enter: "));
-$sorting_type = readline("Choose sorting type: enter 'asc' for ascending, 'desc' for descending: ");
-sorting_array($array, $sorting_type);
-print_r($array);
